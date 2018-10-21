d924947d411e6ccab1f26dc1960cb8927887940a
fuyutarow <fuyutarow@gmail.com>
2018-10-21 20:19:07 +0900

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
+import '@babel/polyfill';
 import Vue from 'vue';
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+
+Vue.use(Vuetify);
+
 
 new Vue({
   router,
