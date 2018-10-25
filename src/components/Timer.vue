67576baeae605f03e6578fc299819c515d3242dd
fuyutarow <fuyutarow@gmail.com>
2018-10-25 11:00:58 +0900

    Add btn stop and notelink

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,26 +1,7 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
-      <v-flex xs12>
-        <h1 style="font-size:100px">
-          {{ formatTime }}
-        </h1>
-      </v-flex>
-      {{ timer }}
-      <v-flex mb-4>
-        <template v-if="timer.inPause">
-          <v-btn @click="play" fab dark>
-            <i class="material-icons">play_arrow</i>
-          </v-btn>
-        </template>
-        <template v-else>
-          <v-btn @click="pause" fab dark>
-            <i class="material-icons">pause</i>
-          </v-btn>
-        </template>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <h1 style="font-size:100px">
+    {{ formatTime }}
+  </h1>
 </template>
 
 <script lang='ts'>
@@ -45,12 +26,6 @@ export default class Timer extends Vue {
       .map((v) => v.toString())
       .map((str) => (str.length < 2) ? '0' + str : str)
       .join(':');
-  }
-  public play() {
-    this.$store.dispatch('timer/play');
-  }
-  public pause() {
-    this.$store.dispatch('timer/pause');
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
-  <Timer />
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex xs12>
+        <Timer />
+      </v-flex>
+      <v-flex mb-4>
+        <v-btn @click="stop" fab dark>
+          <i class="material-icons">stop</i>
+        </v-btn>
+        <template v-if="timer.inPause">
+          <v-btn @click="play" fab dark>
+            <i class="material-icons">play_arrow</i>
+          </v-btn>
+        </template>
+        <template v-else>
+          <v-btn @click="pause" fab dark>
+            <i class="material-icons">pause</i>
+          </v-btn>
+        </template>
+        <v-btn fab dark :to="{
+          name: 'pomodoroDetail',
+          params: { timestamp: lastPomodoro.timestamp},
+          }">
+          <v-icon class="material-icons">create</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -14,5 +41,31 @@ import Timer from '@/components/Timer.vue';
     Timer,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  get pomodoroList() {
+    return this.$store.state.pomodoroList.all;
+  }
+  get lastPomodoro() {
+    return (this.pomodoroList.length < 2) ? this.pomodoroList[0] :
+      this.pomodoroList
+      .filter((a) => a.color !== 'white')
+      .reduceRight((a) => a);
+  }
+  get timer() {
+    return this.$store.state.timer;
+  }
+  public play() {
+    this.$store.dispatch('timer/play');
+  }
+  public pause() {
+    this.$store.dispatch('timer/pause');
+  }
+  public stop() {
+    this.$store.dispatch('timer/pause');
+    this.$store.dispatch('timer/setTimer', {
+      mm: 25,
+      ss: 0,
+    });
+  }
+}
 </script>
