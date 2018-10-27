886f2739e9b6313b5733270286a7e372c058cea6
fuyutarow <fuyutarow@gmail.com>
2018-10-28 00:58:18 +0900

    Change button size. smaller

--- src/views/Clock/index.vue
+++ src/views/Clock/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <v-container>
-    <v-layout text-xs-center wrap>
+  <v-container align-center>
+    <v-layout text-xs-center>
       <v-flex xs12>
         <Timer :color='color' :progress='progress' />
       </v-flex>
-      <v-flex mb-4>
-        <v-btn @click="stop" fab dark>
+    </v-layout>
+    <v-layout text-xs-center>
+      <v-flex>
+        <v-btn @click="stop" fab dark small>
           <i class="material-icons">stop</i>
         </v-btn>
         <template v-if="timer.inPause">
-          <v-btn @click="play" fab dark>
+          <v-btn @click="play" fab dark small>
             <i class="material-icons">play_arrow</i>
           </v-btn>
         </template>
         <template v-else>
-          <v-btn @click="pause" fab dark>
+          <v-btn @click="pause" fab dark small>
             <i class="material-icons">pause</i>
           </v-btn>
         </template>
         <template v-if="this.lastPomodoro">
-          <v-btn fab dark :to="{
+          <v-btn fab dark small :to="{
           name: 'pomodoroDetail',
           params: { timestamp: lastPomodoro.timestamp},
           }">
