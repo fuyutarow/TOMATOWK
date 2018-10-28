aea1d132b16b791d97a6ea2402f572f65372d489
fuyutarow <fuyutarow@gmail.com>
2018-10-28 16:32:09 +0900

    Add show btn

--- src/components/VineItem.vue
+++ src/components/VineItem.vue
@@ -4,10 +4,20 @@
       <Pomodot :color="pomodoro.color" />
       <span> {{datetime}} </span>
     </v-card-title>
-    <v-card-text>
-      <span @click='done=false'><VueMarkdown :source="pomodoro.message" v-show="done" /></span>
-      <Editor v-model:value="pomodoro.message" @done='done=true' v-show="!done" />
-    </v-card-text>
+    <template v-if='done'>
+      <v-card-text>
+        <span @click='done=false'><VueMarkdown :source="pomodoro.message" v-show="done" /></span>
+      </v-card-text>
+    </template>
+    <template v-else>
+      <v-card-text>
+        <Editor v-model:value="pomodoro.message" @done='done=true' v-show="!done" />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn small flat color="orange">show</v-btn>
+      </v-card-actions>
+    </template>
   </v-card>
 </template>
 
