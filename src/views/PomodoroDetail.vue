e5960060553d406612742d2973a01168a8dde603
fuyutarow <fuyutarow@gmail.com>
2018-10-28 19:32:24 +0900

    Use components/PomodoroCard in views/PomodoroDetail

--- src/views/PomodoroDetail.vue
+++ src/views/PomodoroDetail.vue
@@ -1,20 +1,11 @@
 <template>
-  <div>
-    <v-card color="transparent">
-      <v-container fluid grid-list-lg>
-        <v-layout row wrap>
-          <v-flex xs12>
-            <PomodoroCard :pomodoro="pomodoro" :setDone='false' />
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-    <div style="text-align: center;">
-      <v-btn :to="{ name: 'vine' }" slot="activator" color="orange" small flat dark>
-        more
-      </v-btn>
-    </div>
-  </div>
+  <v-container fluid grid-list-lg>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <PomodoroCard :pomodoro="pomodoro" :setDone='false' />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang='ts'>
