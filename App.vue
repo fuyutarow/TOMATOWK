5cfdd0d831b78793072157009f4c02eca598a27b
fuyutarow <fuyutarow@gmail.com>
2018-11-08 01:09:32 +0900

    Hello, vue-native

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+    <text class="text-color-primary">Hello Vue Native App</text>
+  </view>
+</template>
+
+<script>
+export default {}
+</script>
+>
+
+<style>
+.container {
+  background-color: white;
+  flex: 1;
+  padding-top: 40;
+}
+
+.todo-wrapper {
+  margin-horizontal: 15;
+  background-color: beige;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  margin-vertical: 2.5;
+  padding: 10;
+  border-radius: 5;
+}
+
+</style>
--- src/components/Editor/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div id="editor">
-    <textarea :value="text" :style="style" @input='_input' @keydown.tab.prevent="tabber" @keydown.enter.prevent="enterer" @focusout="$emit('done', true)" v-focus></textarea>
-  </div>
-</template>
-
-<script lang='ts' src='./script' />
